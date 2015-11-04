e81f39feec272c120015761fad88140983b4d63d
r28 <r28@users.noreply.github.com>
2015-11-04 18:53:01 +0900

    Any Text into Array

--- src/app.js
+++ src/app.js
@@ -68,34 +68,50 @@ var sunText = new UI.Text({
   text: ''
 });
 
-window.add(pointText);
-window.add(dtText);
-window.add(tempText);
-window.add(rainText);
-window.add(windDirText);
-window.add(windSpeedText);
-window.add(sunText);
+var uiTextArr = {
+  point: pointText,
+  dt:    dtText,
+  temp:  tempText,
+  rain:  rainText,
+  windDir:   windDirText,
+  windSpeed: windSpeedText,
+  sun:   sunText,
+};
+windowAdd(window, uiTextArr);
 window.show();
 
-navigator.geolocation.getCurrentPosition(function(position) {
-  var lat = position.coords.latitude;
-  var lng = position.coords.longitude;
-  var baseUrl = 'http://redmagic.cc/amedas/data/point-data';
-  var apiUrl = baseUrl + '?lat=' + lat + '&lng=' + lng;
+function windowAdd(windowObj, objArr) {
+  console.log('windowAdd');
+  for (var key in objArr) {
+    windowObj.add(objArr[key]);
+  }
+}
 
-  var req = new XMLHttpRequest();
-  req.onload = function () {
-    var response = JSON.parse(req.responseText);
-    console.log('[amedas] Name:' + response.point.name);
-    pointText.text(response.point.name);
-    dtText.text(response.datetime);
-    tempText.text('気温:' + response.temperature + ' C');
-    rainText.text('降水:' + response.precipitation + ' mm');
-    windDirText.text('風向:' + response.wind_direction + 'の風');
-    windSpeedText.text('風速:' + response.wind_speed + ' m/s');
-    sunText.text('日照: ' + response.sunshine + ' hour');
-  };
-  req.open('GET', apiUrl, true);
-  console.log(apiUrl);
-  req.send();
-});
+getCurrentAmedas(uiTextArr);
+
+function getCurrentAmedas(objArr) {
+  navigator.geolocation.getCurrentPosition(function(position) {
+    var lat = position.coords.latitude;
+    var lng = position.coords.longitude;
+    var baseUrl = 'http://redmagic.cc/amedas/data/point-data';
+    var apiUrl = baseUrl + '?lat=' + lat + '&lng=' + lng;
+
+    var req = new XMLHttpRequest();
+    req.onload = function () {
+      var response = JSON.parse(req.responseText);
+      console.log('[amedas] Name:' + response.point.name);
+      pointText.text(response.point.name);
+      dtText.text(response.datetime);
+      
+      objArr.temp.text('気温:' + response.temperature + ' C');
+      objArr.rain.text('降水:' + response.precipitation + ' mm');
+      objArr.windDir.text('風向:' + response.wind_direction + 'の風');
+      objArr.windSpeed.text('風速:' + response.wind_speed + ' m/s');
+      objArr.sun.text('日照: ' + response.sunshine + ' hour');
+    };
+    req.open('GET', apiUrl, true);
+    console.log(apiUrl);
+    req.send();
+  });
+}
+
